4a9b2864c5e97c98143ac813f84f63959cd542de
ShuoHuan <147470197+ShuoHuan@users.noreply.github.com>
2023-10-10 12:35:44 +0800

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BasicPrograms
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
